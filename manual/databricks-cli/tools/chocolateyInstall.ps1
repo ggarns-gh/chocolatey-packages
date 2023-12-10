605a86d8dd1a1b25bef96c7acfa7b6bda0c56fb1
ggarns-gh <70338276+ggarns-gh@users.noreply.github.com>
2023-12-10 12:19:34 +0000

    Updates for v0.210.0 and change licenseUrl

--- manual/databricks-cli/tools/chocolateyInstall.ps1
+++ manual/databricks-cli/tools/chocolateyInstall.ps1
@@ -6,11 +6,8 @@ $toolsDir    = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 $packageArgs = @{
     packageName    = $packageName
     unzipLocation  = $toolsDir
-    url            = 'https://github.com/databricks/cli/releases/download/v0.209.1/databricks_cli_0.209.1_windows_386.zip'
-    url64bit       = 'https://github.com/databricks/cli/releases/download/v0.209.1/databricks_cli_0.209.1_windows_amd64.zip'
-    checksum       = 'dfab728616c327896a9cecf2ad982a3517d175e2658b0b98b8ab4c5cef69ee17'
-    checksumType   = 'sha256'
-    checksum64     = '49e8d9e1ab091df98ee91cd7949c851bbe441d756cfabcc8066d88e41909342b'
+    url64bit       = 'https://github.com/databricks/cli/releases/download/v0.210.0/databricks_cli_0.210.0_windows_amd64.zip'
+    checksum64     = 'ddf37ac5cb21329968e9937bd95e373197f6ba6b79914c5b8dd0417b1e385262'
     checksumType64 = 'sha256'
 }
 
