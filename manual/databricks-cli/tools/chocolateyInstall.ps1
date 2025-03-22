e9cd8c8f8ba7b39e437dc3aa232440eca9796aeb
ggarns-gh <70338276+ggarns-gh@users.noreply.github.com>
2025-03-22 12:26:44 +0000

    Update-Databricks-CLI-to-v0.244.0

--- manual/databricks-cli/tools/chocolateyInstall.ps1
+++ manual/databricks-cli/tools/chocolateyInstall.ps1
@@ -6,8 +6,8 @@ $toolsDir    = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 $packageArgs = @{
     packageName    = $packageName
     unzipLocation  = $toolsDir
-    url64bit       = 'https://github.com/databricks/cli/releases/download/v0.243.0/databricks_cli_0.243.0_windows_amd64-signed.zip'
-    checksum64     = 'c055b1bb3106cd47eec84c889a4817bc43beae3054902790d9b8f99039e9c354'
+    url64bit       = 'https://github.com/databricks/cli/releases/download/v0.244.0/databricks_cli_0.244.0_windows_amd64-signed.zip'
+    checksum64     = '1a901209dc60f4df48ffbd30caa2d78a5d29cdcec29457aeca5bdea47d94fc6c'
     checksumType64 = 'sha256'
 }
 
