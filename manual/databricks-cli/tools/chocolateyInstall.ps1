89c6b3cb8002bf6d65d008107a84f5a37e0d7633
ggarns-gh <70338276+ggarns-gh@users.noreply.github.com>
2026-06-12 17:50:53 +0100

    Revert "Update-Databricks-CLI-to-v1.2.1"
    This reverts commit a9d72af9dd1ff42bc9baae60532663e82bc4e8c9.

--- manual/databricks-cli/tools/chocolateyInstall.ps1
+++ manual/databricks-cli/tools/chocolateyInstall.ps1
@@ -6,8 +6,8 @@ $toolsDir    = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 $packageArgs = @{
     packageName    = $packageName
     unzipLocation  = $toolsDir
-    url64bit       = 'https://github.com/databricks/cli/releases/download/v1.2.1/databricks_cli_1.2.1_windows_amd64.zip'
-    checksum64     = 'a8a26891932c554736efa045a9546a3df4167ebca88a3cdff9bc4e0b3578e36a'
+    url64bit       = 'https://github.com/databricks/cli/releases/download/v1.2.0/databricks_cli_1.2.0_windows_amd64.zip'
+    checksum64     = 'd649ee26b93e7790e7356aabe50b6e65db559d9cfd98d5582678f4998730884d'
     checksumType64 = 'sha256'
 }
 
